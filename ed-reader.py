d1d585032ea8a9a77f6e1c12bb7969a7a8b48f58
amslocal <amslocal@pcamsvc02.dyndns.cern.ch>
2019-06-21 16:04:17 +0000

    Create and copy the file ./buffer/ss_0123456789.png

--- ed-reader.py
+++ ed-reader.py
@@ -4,27 +4,37 @@ import subprocess as subp
 import shlex
 import time 
 
-DEST_PATH="/home/amslocal/src/ams-website__mount/files/EventDisplay/buffer/"
-SCROT_CMD = "scrot ./ss_current.png -q10"
+ROOT_PATH="/home/amslocal/src/ams-website__mount/files/EventDisplay"
+DEST_PATH=ROOT_PATH + '/buffer'
+SCROT_CMD = "scrot ./ss_current.png"
+
+# read the configuration
+import configparser as cfgp
+config = cfgp.ConfigParser()
+config.read(ROOT_PATH+'/config.ini')
+if 'DEFAULT' in config:
+    scrot_key_q = int(config['DEFAULT']['ScreenshotQuality'])
+else:
+    scrot_key_q = 10
+print("CONFIG: q = " + str(scrot_key_q))
+
+
 
 # read the screen and save to a file
 while True:
-    ts = time.gmtime()
+    ts = int(time.time()) #round(time.time(),0) #time.gmtime()
+    print("ts = ", ts)
     
-    #buffer_name = time.strftime("buffer_%Y_%m_%d_%H_%M", ts)
-    #file_name = time.strftime("ss_%S.png", ts)
-    #buffer_path = DEST_PATH+'/'+buffer_name
     subp.Popen(['mkdir', '-p', DEST_PATH])
     file_path = DEST_PATH+'/ss_{0}.png'.format(ts)
-    cmd = "{0} -e 'mv $f {1}'".format(SCROT_CMD, file_path)
-    print(cmd)
+    cmd = "{0} -q{2} -e 'mv $f {1}'".format(SCROT_CMD, file_path, scrot_key_q)
     cmd_seq = shlex.split(cmd)
-    print(cmd_seq)
-    
+    # print(cmd_seq)
+
     p = subp.Popen(cmd_seq)
     pout, perr = p.communicate()
-    print(pout)
-    print(perr)
+    # print(pout)
+    # print(perr)
     time.sleep(1)
     
 
